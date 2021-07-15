fdc2b48575c17d72d25efcc0ea8208b4f819c76c
Steve Hunger <9846101+shunger@users.noreply.github.com>
2021-07-15 17:42:33 -0400

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/shunger-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = ${prefix}
+  bucket_prefix = var.prefix
 }
